bdb2c40a0d9610a16d28770077b54dcafe5c9228
KChimi <260343942+k-chimi@users.noreply.github.com>
2026-03-26 21:57:34 +0900

    fix: fix sorting recent posts procedure

--- .vitepress/datas/posts.data.ts
+++ .vitepress/datas/posts.data.ts
@@ -1,23 +1,24 @@
 import { createContentLoader } from "vitepress";
 
-function parseYearMonth(path: string): { year: number, month: number } {
-  const m = path.match(/\/posts\/(\d+)\/(\d+)/);
-  if (!m) return { year: 0, month: 0 };
+function parseYearMonth(path: string) {
+  const m = path.match(/\/posts\/(\d+)\/(\d+)\/(\d+)/);
+  if (!m) return { year: 0, month: 0, day: 0 };
 
   const year = +m[1].replace(/^0+/, "");
   const month = +m[2].replace(/^0+/, "");
+  const day = +m[3].replace(/^0+/, "");
 
-  return { year, month }
+  return { year, month, day }
 }
 
 export default createContentLoader("posts/**/*.md", {
   transform(data) {
     return data.sort((a, b) => {
-      const { year: aYear, month: aMonth } = parseYearMonth(a.url);
-      const { year: bYear, month: bMonth } = parseYearMonth(b.url);
+      const { year: aYear, month: aMonth, day: aDay } = parseYearMonth(a.url);
+      const { year: bYear, month: bMonth, day: bDay } = parseYearMonth(b.url);
 
-      const ascore = aYear * 12 + aMonth % 12;
-      const bscore = bYear * 12 + bMonth % 12;
+      const ascore = aYear * 12 * 32 + (aMonth % 12) * 32 + aDay;
+      const bscore = bYear * 12 * 32 + (bMonth % 12) * 32 + bDay;
 
       return bscore - ascore;
     }).slice(0, 100)
